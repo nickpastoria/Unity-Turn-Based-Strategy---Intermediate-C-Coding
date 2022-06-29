d8f86d14f0528af41ef4cc68e06da4d89f4ef57e
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-06-29 14:28:29 -0700

    Added MouseWorld Raycast and Debugging

--- Turn Based Strategy Intermediate Coding/Assets/Scripts/MouseWorld.cs
+++ Turn Based Strategy Intermediate Coding/Assets/Scripts/MouseWorld.cs
@@ -7,7 +7,10 @@ public class MouseWorld : MonoBehaviour
     private void Update()
     {
         Debug.Log(Input.mousePosition);
-        
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit);
+        Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
     }
 
 }
