074c699f482b126ce55a0ae9099b6d1f6175b90a
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-06-29 13:38:55 -0700

    Basic Unit Movement from starting position to another random position

---
+++ Turn Based Strategy Intermediate Coding/Assets/Scripts/Unit.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
+    private void Update()
+    {
+        Move();
+
+        if(Input.GetKeyDown(KeyCode.T)) 
+        {
+            SetMoveTarget(new Vector3(4, 0, 4));
+        }
+    }
+
+    private void Move()
+    {
+        if (IsAtTargetPosition() && isMoving) 
+        {
+            transform.position = targetPosition;
+            isMoving = false;
+            return;
+        }
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        transform.position += moveDirection * this.moveSpeed * Time.deltaTime;
+    }
+
+    private void SetMoveTarget(Vector3 targetPosition) 
+    {
+        this.targetPosition = targetPosition;
+        isMoving = true;
+    }
+
+    private bool IsAtTargetPosition()
+    {
+        float stoppingDistance = 0.1f;
+        return Vector3.Distance(transform.position, targetPosition) < stoppingDistance;
+    }
+}
