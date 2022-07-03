487aa20f966db7c21b7d452010f0e6fd87d19dbc
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-07-03 16:47:05 -0700

    Prediction Of Unit Selection

--- Turn Based Strategy Intermediate Coding/Assets/Scripts/UnitSelection.cs
+++ Turn Based Strategy Intermediate Coding/Assets/Scripts/UnitSelection.cs
@@ -6,15 +6,40 @@ public class UnitSelection : MonoBehaviour
 {
 
     [SerializeField] private Unit selectedUnit;
+
+    [SerializeField] private LayerMask mouseLayerMask;
     // Update is called once per frame
     void Update()
     {
-        int mouseButton = 0;
-        bool leftMouseDown = Input.GetMouseButtonDown(mouseButton);
+        int leftMouse = 0;
+        int rightMouse = 1;
+        bool leftMouseDown = Input.GetMouseButtonDown(leftMouse);
+        bool rightMouseDown = Input.GetMouseButtonDown(rightMouse);
 
         if(leftMouseDown)
         {
-            selectedUnit.setWalkTarget(MouseWorld.GetPosition());
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, this.mouseLayerMask);
+            if(hit.collider != null)
+            {
+                Unit unit = hit.collider.GetComponent<Unit>();
+                if(unit != null)
+                {
+                    this.selectedUnit = unit;
+                }
+            }
+            else 
+            {
+                this.selectedUnit = null;
+            }
+        }
+
+        if(rightMouseDown)
+        {
+            if(this.selectedUnit != null)
+            {
+                this.selectedUnit.setWalkTarget(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            }
         }
     }
 }
